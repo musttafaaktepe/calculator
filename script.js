34c710be233f6dfe72a4262a3656434f3cb8a21f
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-09-28 13:09:33 +0300

    js

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let num2;
 let operator;
 let num1Done = false;
 let floating = false;
+
 btnContainer.addEventListener("click", (e) => {
     if(e.target.id == "n0"){
         input.innerText += 0;
@@ -28,32 +29,67 @@ btnContainer.addEventListener("click", (e) => {
       } else if (e.target.id == "n9") {
         input.innerText += 9;
       } else if (e.target.id == "division" && !num1Done && input.innerText) {
-        input2.innerText += input.innerText + " ÷";
+        input2.innerText += input.innerText + " ÷";         
         num1 = Number(input.innerText);
         operator = "division";
         input.innerText = "";
         num1Done = true;
         floating = false;
       } else if (e.target.id == "multi" && !num1Done && input.innerText) {
-        input2.innerText += input.innerText + " x";
+        input2.innerText += input.innerText + " x";         
         num1 = Number(input.innerText);
         operator = "multi";
         input.innerText = "";
         num1Done = true;
         floating = false;
       } else if (e.target.id == "sub" && !num1Done && input.innerText) {
-        input2.innerText += input.innerText + " -";
+        input2.innerText += input.innerText + " -";         
         num1 = Number(input.innerText);
         operator = "sub";
         input.innerText = "";
         num1Done = true;
         floating = false;
       } else if (e.target.id == "add" && !num1Done && input.innerText) {
-        input2.innerText += input.innerText + " +";
+        input2.innerText += input.innerText + " +";         
         num1 = Number(input.innerText);
         operator = "add";
         input.innerText = "";
         num1Done = true;
+        floating = false;
+      } else if(e.target.id == "dec" && !floating){
+        input.innerText += ".";
+        floating = true;
+      }else if (e.target.id == "equal" && num1Done && input.innerText){
+        num2 = Number(input.innerText);
+        num1Done = false; 
+        floating = false;
+        input2.innerText = ""; 
+        switch (operator) {         
+            case "division":
+              input.innerText = num1 / num2;
+              break;
+            case "add":
+              input.innerText = num1 + num2;
+              break;
+            case "sub":
+              input.innerText = num1 - num2;
+              break;
+            case "multi":
+              input.innerText = num1 * num2;
+              break;
+          }
+      }else if (e.target.id == "per" && input.innerText) {
+        input.innerText = Number(input.innerText) / 100;        
+      }else if (e.target.id == "pm" && input.innerText){
+        if (input.innerText.includes("-")) { 
+            input.innerText = input.innerText.replace("-", "");
+      }else {
+        input.innerText = "-" + input.innerText;              
+      }
+    }else if (e.target.id == "ac"){      
+        input.innerText = "";
+        input2.innerText = "";
+        num1Done = false;
         floating = false;
       }
 });
