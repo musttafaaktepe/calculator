22705316e9b406f061c29e0509db98c18771b246
musttafaaktepe@gmail.com <musttafaaktepe@gmail.com>
2022-09-28 10:17:58 +0300

    button1JS

---
+++ script.js
@@ -0,0 +1,39 @@
+const input = document.querySelector(".input");
+const input2 = document.querySelector(".input2");
+const btnContainer = document.querySelector(".button-container");
+
+let num1;
+let num2;
+let operator;
+let num1done = false;
+let floating = false;
+
+btnContainer.addEventListener("click", (e) => {
+  if (e.target.id == "n0") {
+    if (input.innerText == ``) {
+      input.innerText = 0;
+    } else if (input.innerText == `0`) {
+    } else {
+      input.innerText += 0;
+    }
+  } else if (e.target.id == "n1") {
+    input.innerText += 1;
+  } else if (e.target.id == "n2") {
+    input.innerText += 2;
+  } else if (e.target.id == "n3") {
+    input.innerText += 3;
+  } else if (e.target.id == "n4") {
+    input.innerText += 4;
+  } else if (e.target.id == "n5") {
+    input.innerText += 5;
+  } else if (e.target.id == "n6") {
+    input.innerText += 6;
+  } else if (e.target.id == "n7") {
+    input.innerText += 7;
+  } else if (e.target.id == "n8") {
+    input.innerText += 8;
+  } else if (e.target.id == "n9") {
+    input.innerText += 9;
+  } else if (e.target.id == "division" && !num1done && input.innerText) {
+  }
+});
